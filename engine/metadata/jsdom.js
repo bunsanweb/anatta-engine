ea27265a95d45df636b363cd9bc385e8f94d842a
bellbind <bellbind@gmail.com>
2012-10-26 16:15:56 +0900

    [add] mocha test base [add] .gitignore [update] package.json for limited dependencies
    	new file:   .gitignore
    	new file:   engine/metadata/jsdom.js
    	modified:   package.json
    	new file:   test/metadata-api-test.js
    	new file:   test/mocha.opts

---
+++ engine/metadata/jsdom.js
@@ -0,0 +1,46 @@
+"use strict";
+
+var jsdom = require("jsdom");
+var features = jsdom.defaultDocumentFeatures = {
+    FetchExternalResources: [],
+    ProcessExternalResources: [],
+    MutationEvents: false,
+    QuerySelector: true,
+};
+var browser = jsdom.browserAugumentation(jsdom.dom.level3.html, {
+    features: features,
+});
+
+// compat function for "document.implementation.createHTMLDocument"
+var createHTMLDocument = exports.createHTMLDocument = function (title) {
+    var html = jsdom.jsdom(
+        "<!doctype html><html><head></head><body></body></html>",
+        jsdom.dom.level3.html, {features: jsdomFeatures});
+    if (typeof title === "string") {
+        var titleNode = html.createElement("title");
+        titleNode.textContent = title;
+        html.head.appendChild(titleNode);
+    }
+    if (!html.implementation.createHTMLDocument) {
+        html.implementation.createHTMLDocument = createHTMLDocument;
+    }
+    return html;
+};
+
+// compat function for "document.implementation.createDocument"
+var createDocument = exports.createDocument = function () {
+    var xml = jsdom.jsdom(
+        "", jsdom.dom.level3.core, {features: jsdomFeatures});
+    if (!xml.implementation.createDocument) {
+        xml.implementation.createDocument = createDocument;
+    }
+    return xml;
+};
+
+// compat object for XMLSerializer
+var XMLSerializer = exports.XMLSerializer = function () {
+    return Object.create(XMLSerializer.prototype);
+};
+XMLSerializer.prototype.serializeToString = function (node) {
+    return node.outerHTML;
+};
---
+++ test/metadata-api-test.js
@@ -0,0 +1,5 @@
+
+suite("Matadata API");
+test("Example: Pick metadata from HTML", function (done) {
+    done();
+});
