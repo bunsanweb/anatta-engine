d710ae3b252c0f6e69d89481a38296d0d606cca8
bellbind <bellbind@gmail.com>
2013-01-18 15:23:09 +0900

    [add] HTML5 style multipart/form-data support
    - see: note of 4.10.22.6 - 5 at http://www.w3.org/TR/html51/forms.html
    
    > the "sets of files" feature ("multipart/mixed") of RFC 2388 is not used
    
    .
    
    	modified:   engine/metadata/multipart.js
    	new file:   test/metadata-multipart5-test.js
    	modified:   test/mocha.opts

--- engine/metadata/multipart.js
+++ engine/metadata/multipart.js
@@ -2,21 +2,39 @@
 
 // mulipart/form-data decoder/encoder
 //
-// see: http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.2
+// see html4: http://www.w3.org/TR/html401/interact/forms.html#h-17.13.4.2
+// see html5: note of 4.10.22.6 - 5 at http://www.w3.org/TR/html51/forms.html
+// > the "sets of files" feature ("multipart/mixed") of RFC 2388 is not used.
 
+// [html5 style formdata]
+// multipart object structure as:
+// {
+//   stringKey: stringValue, ...,
+//   filesKey: [{filename: "...", headers: {...}, body: Buffer(...)}, ...],
+//   ...,
+// }
+//
+// [module usage]
+//  message = {headers: {"content-type": "multipart/form-data; boundary=..."
+//  }, body: Buffer(...)}
+//  multipartObject = mod.decode5(message)
+//  message = mod.encode5(multipartObject)
+
+// [HTML4 style formdata]
 // multipart object structure as:
 // {
 //   stringKey: stringValue, ...,
 //   fileKey: {filename: "...", headers: {...}, body: Buffer(...)}, ...,
-//   filesKey: [{filename: "...", headers: {...}, body: Buffer(...)}, ...], 
+//   filesKey: [{filename: "...", headers: {...}, body: Buffer(...)}, ...],
 //   ...,
 // }
-
-//[module usage]
+//
+// [module usage]
 //  message = {headers: {"content-type": "multipart/form-data; boundary=..."
 //  }, body: Buffer(...)}
 //  multipartObject = mod.decode(message)
 //  message = mod.encode(multipartObject)
+
 
 var crypto = require("crypto");
 
@@ -36,6 +54,13 @@ var decodeMultipart = function (message) {
     if (!contentType.match(/^multipart\/form-data/)) return null;
     var boundary = getBoundary(contentType);
     return parseMultipart(message.body.toString("binary"), boundary);
+};
+
+var decodeMultipart5 = function (message) {
+    var contentType = message.headers["content-type"];
+    if (!contentType.match(/^multipart\/form-data/)) return null;
+    var boundary = getBoundary(contentType);
+    return parseMultipart5(message.body.toString("binary"), boundary);
 };
 
 var parseMultipart = function (body, boundary) {
@@ -58,11 +83,44 @@ var parseMultipart = function (body, boundary) {
         } else if (contentType.match(/^multipart\/mixed;/)) {
             result[name] = parseMixed(disposition);
         } else {
-            result[name] = parseFile(disposition);
+            var file = parseFile(disposition);
+            if (!result[name]) result[name] = file;
+            else if (Array.isArray(result[name])) result[name].push(file);
+            else result[name] = [result[name], file];
         }
         return result;
     }, {});
 };
+
+var parseMultipart5 = function (body, boundary) {
+    if (body.substring(body.length - 2) === "\r\n") {
+        body = body.substring(0, body.length - 2);
+    }
+    var dispositions = splitMultipart(body, boundary).map(function (part) {
+        return parseDisposition(part);
+    }).filter(function (disposition) {return disposition;});
+    
+    return dispositions.reduce(function (result, disposition) {
+        var type = disposition.headers["content-disposition"];
+        var contentType = disposition.headers["content-type"];
+        if (!type.match(/^form-data/)) return result;
+        
+        var name = Buffer(
+            type.match(/\bname="([^"]+)"/)[1], "binary").toString();
+        if (!contentType) {
+            result[name] = new Buffer(disposition.body, "binary").toString();
+        } else if (contentType.match(/^multipart\/mixed;/)) {
+            result[name] = parseMixed(disposition);
+        } else {
+            var file = parseFile(disposition);
+            if (!result[name]) result[name] = [file];
+            else if (Array.isArray(result[name])) result[name].push(file);
+            else result[name] = [result[name], file];
+        }
+        return result;
+    }, {});
+};
+
 
 var parseFile = function (disposition) {
     var type = disposition.headers["content-disposition"];
@@ -143,6 +201,31 @@ var encodeMultipart = function (obj) {
     return {headers: headers, body: Buffer(body, "binary")};
 };
 
+var encodeMultipart5 = function (obj) {
+    var bodies = Object.keys(obj).reduce(function (bodies, key) {
+        var value = obj[key];
+        var binkey = Buffer(key).toString("binary");
+        if (typeof value === "string") {
+            bodies.push(encodeKeyValue(binkey, value));
+        } else if (Array.isArray(value)) {
+            value.forEach(function (file) {
+                bodies.push(encodeSingleFile(binkey, file));
+            });
+        } else bodies.push(encodeSingleFile(binkey, value));
+        return bodies;
+    }, []);
+    var boundary = makeBoundary(bodies);
+    var sep = "--" + boundary;
+    var body = bodies.reduce(function (buf, body) {
+        return buf + "\r\n" + body + "\r\n" + sep;
+    }, sep) + "--\r\n";
+    
+    var headers = {
+        "content-type": "multipart/form-data; boundary=" + boundary,
+    };
+    return {headers: headers, body: Buffer(body, "binary")};
+};
+
 var encodeMessage = function (headers, body) {
     var headerPart = Object.keys(headers).reduce(function (part, key) {
         var line = key + ": " + headers[key]; //TBD: RFC5987 for non-ascii vals
@@ -207,3 +290,5 @@ var makeBoundary = function (bodies) {
 
 exports.encode = encodeMultipart;
 exports.decode = decodeMultipart;
+exports.encode5 = encodeMultipart5;
+exports.decode5 = decodeMultipart5;
---
+++ test/metadata-multipart5-test.js
@@ -0,0 +1,176 @@
+"use strict";
+
+var assert = require("assert");
+
+suite("[multipart encode5/decode5]");
+test("encode5 and decode5", function () {
+    var anatta = require("../anatta");
+    
+    var obj = {
+        "usual1": "value1",
+        "usual2": "value2",
+        "multi": [
+            {
+                filename: "index.html",
+                headers: {
+                    "content-type": "text/html;charset=utf-8",
+                },
+                body: Buffer("<body><script src='script.js'></script></body>"),
+            },
+            {
+                filename: "script.js",
+                headers: {
+                    "content-type": "text/javascript",
+                },
+                body: Buffer("document.write('Hello World');"),
+            },
+        ],
+    };
+    var msg = anatta.metadata.multipart.encode5(obj);
+    //console.log(msg.headers["content-type"]);
+    //console.log(msg.body.toString());
+    var decoded = anatta.metadata.multipart.decode5(msg);
+    //console.log(decoded);
+    assert.equal(decoded["usual1"], obj["usual1"]);
+    assert.equal(decoded["usual2"], obj["usual2"]);
+    
+    for (var i = 0; i < 2; i++) {
+        assert.equal(decoded["multi"][i].filename, obj["multi"][i].filename);
+        assert.equal(decoded["multi"][i].body.toString(),
+                     obj["multi"][i].body.toString());
+        assert.equal(decoded["multi"][i].headers["content-type"],
+                     obj["multi"][i].headers["content-type"]);
+    }
+});
+
+test("encode5/decode5 with non-ascii utf-8 charset value", function () {
+    var anatta = require("../anatta");
+    var obj = {
+        "usual1": "\u5728",
+        "usual2": "value2",
+        "multi": [
+            {
+                filename: "index.html",
+                headers: {
+                    "content-type": "text/html;charset=utf-8",
+                },
+                body: Buffer("<body><script src='script.js'></script></body>"),
+            },
+            {
+                filename: "script.js",
+                headers: {
+                    "content-type": "text/javascript",
+                },
+                body: Buffer("document.write('\u5708');"),
+            },
+        ],
+    };
+    var msg = anatta.metadata.multipart.encode5(obj);
+    //console.log(msg.headers["content-type"]);
+    //console.log(msg.body.toString());
+    var decoded = anatta.metadata.multipart.decode5(msg);
+    //console.log(decoded);
+    assert.equal(decoded["usual1"], obj["usual1"]);
+    assert.equal(decoded["usual2"], obj["usual2"]);
+    
+    for (var i = 0; i < 2; i++) {
+        assert.equal(decoded["multi"][i].filename, obj["multi"][i].filename);
+        assert.equal(decoded["multi"][i].body.toString(),
+                     obj["multi"][i].body.toString());
+        assert.equal(decoded["multi"][i].headers["content-type"],
+                     obj["multi"][i].headers["content-type"]);
+    }
+});
+
+test("encode5/decode5 with non-ascii utf-8 charset key", function () {
+    var anatta = require("../anatta");
+    
+    var obj = {
+        "\u5728": "value1",
+        "usual2": "value2",
+        "\u5708": [
+            {
+                filename: "index.html",
+                headers: {
+                    "content-type": "text/html;charset=utf-8",
+                },
+                body: Buffer("<body><script src='script.js'></script></body>"),
+            },
+            {
+                filename: "script.js",
+                headers: {
+                    "content-type": "text/javascript",
+                },
+                body: Buffer("document.write('Hello World');"),
+            },
+        ],
+    };
+    var msg = anatta.metadata.multipart.encode5(obj);
+    //console.log(msg.headers["content-type"]);
+    //console.log(msg.body.toString());
+    var decoded = anatta.metadata.multipart.decode5(msg);
+    //console.log(decoded);
+    assert.equal(decoded["\u5728"], obj["\u5728"]);
+    assert.equal(decoded["usual2"], obj["usual2"]);
+    
+    for (var i = 0; i < 2; i++) {
+        assert.equal(decoded["\u5708"][i].filename, obj["\u5708"][i].filename);
+        assert.equal(decoded["\u5708"][i].body.toString(),
+                     obj["\u5708"][i].body.toString());
+        assert.equal(decoded["\u5708"][i].headers["content-type"],
+                     obj["\u5708"][i].headers["content-type"]);
+    }
+});
+
+test("encode and decode with non-ascii filename", function () {
+    var anatta = require("../anatta");
+    
+    var obj = {
+        "usual1": "value1",
+        "usual2": "value2",
+        "single": {
+            filename: "\u5728.txt",
+            headers: {
+                "content-type": "text/plain;charset=utf-8",
+            },
+            body: Buffer("Hello World!"),
+        },
+        "multi": [
+            {
+                filename: "\u5727.html",
+                headers: {
+                    "content-type": "text/html;charset=utf-8",
+                },
+                body: Buffer("<body><script src='script.js'></script></body>"),
+            },
+            {
+                filename: "\u5708.js",
+                headers: {
+                    "content-type": "text/javascript",
+                },
+                body: Buffer("document.write('Hello World');"),
+            },
+        ],
+    };
+    var msg = anatta.metadata.multipart.encode(obj);
+    //console.log(msg.headers["content-type"]);
+    //console.log(msg.body.toString());
+    var decoded = anatta.metadata.multipart.decode(msg);
+    //console.log(decoded);
+    assert.equal(decoded["usual1"], obj["usual1"]);
+    assert.equal(decoded["usual2"], obj["usual2"]);
+    
+    assert.equal(decoded["single"].filename, obj["single"].filename);
+    assert.equal(decoded["single"].body.toString(),
+                 obj["single"].body.toString());
+    assert.equal(decoded["single"].headers["content-type"],
+                 obj["single"].headers["content-type"]);
+
+    for (var i = 0; i < 2; i++) {
+        assert.equal(decoded["multi"][i].filename, obj["multi"][i].filename);
+        assert.equal(decoded["multi"][i].body.toString(),
+                     obj["multi"][i].body.toString());
+        assert.equal(decoded["multi"][i].headers["content-type"],
+                     obj["multi"][i].headers["content-type"]);
+    }
+});
