98f1de26e249d0bde099f64f2d9582c1badde9e4
Motoki Naruke <g.motoki@gmail.com>
2013-02-07 17:38:25 +0900

    get paging comment, refs #29
    	modified:   examples/discovery/agent/script.js

--- examples/discovery/agent/script.js
+++ examples/discovery/agent/script.js
@@ -1,5 +1,6 @@
 "use strict";
 window.addEventListener("agent-load", function (ev) {
+    var url = anatta.builtin.url;
     var rootUri = document.getElementById("rootUri").href;
     var root = anatta.engine.link({href: rootUri});
     var container = document.querySelector(".container").cloneNode(true);
@@ -8,18 +9,35 @@ window.addEventListener("agent-load", function (ev) {
 
     var setContainer = function (entity) {
         var item = entity.html.querySelector(".subtext > a[href^='item?']");
-        var itemHref = rootUri + item.getAttribute("href");
-        var title  = item.parentNode.parentNode.previousSibling.querySelector(".title > a");
+        var itemHref = url.resolve(rootUri, item.getAttribute("href"));
+        var itemAncestor = item.parentNode.parentNode;
+        var title = itemAncestor.previousSibling.querySelector(".title > a");
         container.querySelector(".title").textContent = title.textContent;
         container.querySelector(".href").href = itemHref;
         container.querySelector(".href").textContent = itemHref;
     };
 
+    var getPages = function (pages, href) {
+        var link = anatta.engine.link({href: href});
+        return link.get().then(function (entity) {
+            pages.push(entity.html);
+            var more = entity.html.querySelector("a[href^='/x?fnid']");
+            if (more) {
+                var moreHref = more.getAttribute("href");
+                return getPages(pages, url.resolve(rootUri, moreHref));
+            } else {
+                var deferred = anatta.q.defer();
+                deferred.resolve(pages);
+                return deferred.promise;
+            }
+        });
+    };
+
     var getOuterLinks = function (document) {
-        return Array.prototype.filter.call(
-            document.querySelectorAll("a"), function (a) {
-                return a.rel == "nofollow" && a.href.indexOf("http") == 0;
-            });
+        return Array.prototype.filter.call(document.querySelectorAll("a"),
+                function (a) {
+                    return a.rel == "nofollow" && a.href.indexOf("http") == 0;
+                });
     };
 
     var createContent = function (entity) {
@@ -42,11 +60,15 @@ window.addEventListener("agent-load", function (ev) {
         root.get().then(function (entity) {
             setContainer(entity);
             var itemHref = container.querySelector(".href").href;
-            return anatta.engine.link({href: itemHref}).get();
-        }).then(function (entity) {
-            var links = getOuterLinks(entity.html);
-            return anatta.q.all(Array.prototype.map.call(links, function (a) {
-                return anatta.engine.link({href: a.href}).get();
+            return getPages([], itemHref);
+        }).then(function (pages) {
+            var links = pages.map(function (page) {
+                return getOuterLinks(page);
+            }).reduce(function (prev, cur) {
+                return prev.concat(cur);
+            });
+            return anatta.q.all(links.map(function (link) {
+                return anatta.engine.link({href: link.href}).get();
             }));
         }).then(function (entities) {
             entities.forEach(function (entity) {
