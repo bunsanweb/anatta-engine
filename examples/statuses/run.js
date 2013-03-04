15957024b73d2454eaa75e395a3dfa34aea11903
bellbind <bellbind@gmail.com>
2013-03-04 17:22:05 +0900

    use builder, refs #41
    	modified:   examples/statuses/run.js

--- examples/statuses/run.js
+++ examples/statuses/run.js
@@ -2,22 +2,19 @@
 
 var anatta = require("../../anatta");
 
-var engine = anatta.engine.core.Engine();
-engine.porter.map["text/html"] = anatta.metadata.html;
-engine.porter.map["application/json"] = anatta.metadata.json;
-
-engine.space.manager.bind("fileAgent", "file:", anatta.space.file.FileField({
-    root: "./agent/", prefix: "/"}));
-var agentField = anatta.weaver.core.AgentField({
-    uri: "file:/index.html"});
-agentField.agent.engine = engine;
-engine.space.manager.bind("agent", "root:/stream/", agentField);
-
-engine.space.manager.bind("orb", "root:/orb", anatta.orb.core.OrbField());
-
-engine.space.manager.bind("filePub", "root:/", anatta.space.file.FileField({
-    root: "./pub/", prefix: "/"}));
-
+var engine = anatta.engine.builder.engine({
+    type: "generic",
+    porter: {
+        "text/html": "html",
+        "application/json": "json",
+    },
+    space: {
+        "file:": {field: "file", root: "./agent/", prefix: "/"},
+        "root:/": {field: "file", root: "./pub/", prefix: "/"},
+        "root:/stream/": {field: "agent", uri: "file:/index.html"},
+        "root:/orb": {field: "orb"},
+    }
+});
 var gate = anatta.webgate.core.WebGate(
     engine.space, {from: "/", to: "root:/"});
 gate.start(process.env.PORT || "8000");
