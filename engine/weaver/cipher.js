bf078305ea8ab25ce58b637e9a60d68a07a5a500
bellbind <bellbind@gmail.com>
2016-01-26 16:46:01 +0900

    [update] switch from ursa to node-forge
    - ursa has not followed modern node >= 4.0 yet
    - use node-forge for RSA system as
        - key pair generation
        - PEM formatting
        - encrypt/decrypt
        - sign/verify
        - (hash algorithms for signing)
    - keep to use Buffer/string conversion and crypto modules

--- engine/weaver/cipher.js
+++ engine/weaver/cipher.js
@@ -1,41 +1,86 @@
 "use strict";
 
-var crypto = require("crypto");
-var ursa = require("ursa");
+const crypto = require("crypto");
+const forge = require("forge");
 
-var Key = function (key) {
+// forge utilities
+const isPublicKey = (forgeKey) =>
+          forgeKey && !Object.getOwnPropertyDescriptor(forgeKey, "d");
+const isPrivateKey = (forgeKey) => forgeKey && !isPublicKey(forgeKey);
+
+const publicEncrypt = (forgeKey, buf, encoding) => {
+    const raw = buf.toString("binary");
+    const encrypted = forgeKey.encrypt(raw);
+    return Buffer(encrypted, "binary").toString(encoding);
+};
+const privateDecrypt = (forgeKey, str, encoding) => {
+    const raw = Buffer(str, encoding).toString("binary");
+    const decrypted = forgeKey.decrypt(raw);
+    return Buffer(decrypted, "binary");
+};
+const privateEncrypt = (forgeKey, buf, encoding) => {
+    const raw = buf.toString("binary");
+    const encrypted = forge.pki.rsa.encrypt(raw, forgeKey, 1);
+    return Buffer(encrypted, "binary").toString(encoding);
+};
+const publicDecrypt = (forgeKey, str, encoding) => {
+    const raw = Buffer(str, encoding).toString("binary");
+    const decrypted = forge.pki.rsa.decrypt(raw, forgekey, true, true);
+    return Buffer(decrypted, "binary");
+};
+
+const hashAndSign = (forgeKey, alg, buf, encoding) => {
+    const md = forge.md[alg].create().update(buf);
+    const sign = forgeKey.sign(md);
+    return Buffer(sign, "binary").toString(encoding);
+};
+const hashAndVerify = (forgeKey, alg, buf, signStr, encoding) => {
+    const sign = Buffer(signStr, encoding).toString("binary");
+    const md = forge.md[alg].create().update(buf);
+    return forgeKey.verify(md.digest().bytes(), sign);
+};
+
+const publicKeyFromPrivateKey = (forgeKey) =>
+          forge.pki.rsa.setPublicKey(forgeKey.n, forgeKey.e);
+const keyToPem = (forgeKey) => isPrivateKey(forgeKey) ?
+          forge.pki.privateKeyToPem(forgeKey) :
+          forge.pki.publicKeyToPem(forgeKey);
+const keyFromPem = (pem) => {
+    const msg = forge.pem.decode(pem)[0];
+    const obj = forge.asn1.fromDer(msg.body);
+    if (msg.type === "PRIVATE KEY" || msg.type === "RSA PRIVATE KEY") {
+        return forge.pki.privateKeyFromAsn1(obj);
+    } else if (msg.type === "PUBLIC KEY" || msg.type === "RSA PUBLIC KEY") {
+        return forge.pki.publicKeyFromAsn1(obj);
+    }
+    throw Error("The PEM data is not RSA Key");
+};
+
+
+const Key = function (key) {
     return Object.create(Key.prototype, {
         key: {value: key}
     });
 };
 Key.prototype.isPrivateKey = function () {
-    if (!this.key) return false;
-    return ursa.isPrivateKey(this.key);
+    return isPrivateKey(this.key);
 };
 Key.prototype.isPublicKey = function () {
-    if (!this.key) return false;
-    return ursa.isPublicKey(this.key);
+    return isPublicKey(this.key);
 };
 Key.prototype.getPem = function () {
     if (!this.key) return "";
-    if (ursa.isPrivateKey(this.key)) {
-        return this.key.toPrivatePem().toString();
-    } else {
-        return this.key.toPublicPem().toString();
-    }
+    return keyToPem(this.key);
 };
 Key.prototype.encode = function (info) {
-    var encoding = info.encoding || "base64";
-    var rawPass = crypto.randomBytes(info.size || 64);
-    var encoder = crypto.createCipher(info.cipher, rawPass);
+    const encoding = info.encoding || "base64";
+    const rawPass = crypto.randomBytes(info.size || 64);
+    const encoder = crypto.createCipher(info.cipher, rawPass);
     encoder.update(Buffer(info.data));
-    var data = encoder.final(encoding);
-    var pass = "";
-    if (ursa.isPrivateKey(this.key)) {
-        pass = this.key.privateEncrypt(rawPass, "binary", encoding);
-    } else {
-        pass = this.key.encrypt(rawPass, "binary", encoding);
-    }
+    const data = encoder.final(encoding);
+    const pass = isPrivateKey(this.key) ?
+              privateEncrypt(this.key, rawPass, encoding) :
+              publicEncrypt(this.key, rawPass, encoding);
     return {
         cipher: info.cipher,
         encoding: encoding,
@@ -44,45 +89,39 @@ Key.prototype.encode = function (info) {
     };
 };
 Key.prototype.decode = function (info) {
-    var encoding = info.encoding || "base64";
+    const encoding = info.encoding || "base64";
     try {
-        var rawPass = "";
-        if (ursa.isPrivateKey(this.key)) {
-            rawPass = this.key.decrypt(info.pass, encoding, "binary");
-        } else {
-            rawPass = this.key.publicDecrypt(info.pass, encoding, "binary");
-        }
-        var decoder = crypto.createDecipher(info.cipher, rawPass);
+        const rawPass = isPrivateKey(this.key) ?
+                  privateDecrypt(this.key, info.pass, encoding) :
+                  publicDecrypt(this.key, info.pass, encoding);
+        const decoder = crypto.createDecipher(info.cipher, rawPass);
         decoder.update(info.data, encoding);
-        var data = decoder.final();
+        const data = decoder.final();
         return data;
     } catch (err) {
         return null;
     }
 };
 
-var Private = function (key) {
+
+const Private = function (key) {
     return Object.create(Private.prototype, {
         key: {value: key}
     });
 };
 Private.prototype = Key();
 Private.prototype.getPublicPem = function () {
-    return this.key.toPublicPem().toString();
+    return forge.pki.publicKeyToPem(publicKeyFromPrivateKey(this.key));
 };
 Private.prototype.sign = function (info) {
     info.alg = info.alg || "sha256";
     info.bufEncoding = info.bufEncoding || "utf8";
     info.signEncoding = info.signEncoding || "base64";
     try {
-        var buf = info.buf;
-        if (!Buffer.isBuffer(buf)) {
-            buf = new Buffer(buf, info.bufEncoding);
-        } else {
-            info.buf = buf.toString();
-        }
-        info.sign = this.key.hashAndSign(
-                info.alg, buf, undefined, info.signEncoding);
+        const buf = Buffer.isBuffer(info.buf) ? info.buf :
+                  Buffer(info.buf, info.bufEncoding);
+        if (Buffer.isBuffer(info.buf)) info.buf = info.buf.toString();
+        info.sign = hashAndSign(this.key, info.alg, buf, info.signEncoding);
     } catch (err) {
         info.error = err;
         info.sign = "";
@@ -90,48 +129,48 @@ Private.prototype.sign = function (info) {
     return info;
 };
 
-var Public = function (key) {
+const Public = function (key) {
     return Object.create(Public.prototype, {
         key: {value: key}
     });
 };
 Public.prototype = Key();
 Public.prototype.verify = function (info) {
-    var bufEncoding = info.bufEncoding || "utf8";
-    var signEncoding = info.signEncoding || "base64";
+    const bufEncoding = info.bufEncoding || "utf8";
+    const signEncoding = info.signEncoding || "base64";
     try {
-        var buf = info.buf;
-        if (!Buffer.isBuffer(buf)) buf = new Buffer(buf, bufEncoding);
-        return this.key.hashAndVerify(info.alg, buf, info.sign, signEncoding);
+        const buf = Buffer.isBuffer(info.buf) ? info.buf :
+                  Buffer(info.buf, bufEncoding);
+        return hashAndVerify(this.key, info.alg, buf, info.sign, signEncoding);
     } catch (err) {
         return false;
     }
 };
 
-var generate = function (bits, exp) {
+const generate = function (bits, exp) {
     bits = bits || 1024;
     exp = exp || 17;
-    return new Private(ursa.generatePrivateKey(bits, exp));
+    return new Private(forge.pki.rsa.generateKey(bits, exp));
 };
 
-var load = function (pem) {
-    var key = ursa.createKey(pem);
-    if (ursa.isPrivateKey(key)) return new Private(key);
-    if (ursa.isPublicKey(key)) return new Public(key);
+const load = function (pem) {
+    const key = keyFromPem(pem);
+    if (isPrivateKey(key)) return new Private(key);
+    if (isPublicKey(key)) return new Public(key);
 };
 
-var isPrivateKeyPem = function (pem) {
+const isPrivateKeyPem = function (pem) {
     try {
-        var key = load(pem);
+        const key = load(pem);
         return key.isPrivateKey();
     } catch (err) {
         return false;
     }
 };
 
-var isPublicKeyPem = function (pem) {
+const isPublicKeyPem = function (pem) {
     try {
-        var key = load(pem);
+        const key = load(pem);
         return key.isPublicKey();
     } catch (err) {
         return false;
